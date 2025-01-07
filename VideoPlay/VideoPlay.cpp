944546d060e3f78d38ae2f6b0dcab770ca3679d2
ChengYuchao <3414906693@qq.com>
2025-01-07 20:21:56 +0800

    feat：VLC库函数学习

--- VideoPlay/VideoPlay.cpp
+++ VideoPlay/VideoPlay.cpp
@@ -1,21 +1,47 @@
-﻿// VideoPlay.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include "vlc.h"
+#include <Windows.h>
+
+// Unicode -> utf-8
+std::string UnicodeToUtf8(const std::wstring& strIn) {
+    std::string strOut;
+    int nLen = WideCharToMultiByte(CP_UTF8, 0, strIn.c_str(), strIn.size(), NULL, 0, NULL, NULL);
+    strOut.resize(nLen + 1);
+    WideCharToMultiByte(CP_UTF8, 0, strIn.c_str(), strIn.size(), (LPSTR)strOut.c_str(), nLen, NULL, NULL);
+    return strOut;
+}
 
 int main()
 {
+    libvlc_instance_t* vlc_instance = libvlc_new(0, nullptr);
+    std::string filePath = UnicodeToUtf8(L"C:\\Users\\24777\\Videos\\mp4素材\\car.mp4");  // 多字节-> Unicode -> utf-8
+    libvlc_media_t* media = libvlc_media_new_path(vlc_instance, filePath.c_str());
+    libvlc_media_player_t* player = libvlc_media_player_new_from_media(media);
+    do
+    {
+        int ret = libvlc_media_player_play(player);
+        if (ret == -1)
+        {
+            printf("error play!\r\n");
+            break;
+        }
+        Sleep(200); // 只有 media 加载完成后才会有下面的参数
+        libvlc_time_t tm = libvlc_media_player_get_length(player);
+        printf("%02d:%02d:%02d.%03d\r\n", int(tm / 3600000), int((tm / 60000) % 60), int(tm / 1000) % 60, int(tm) % 1000); // 时:分:秒.毫秒
+        int width = libvlc_video_get_width(player);
+        int height = libvlc_video_get_height(player);
+        printf("width:%d height:%d\r\n", width, height);
+        getchar();
+        libvlc_media_player_pause(player); // 暂停
+        getchar();
+        libvlc_media_player_play(player); // 播放
+        getchar();
+        libvlc_media_player_stop(player); // 停止
+    } while (0);
+
+    libvlc_media_player_release(player);
+    libvlc_media_release(media);
+    libvlc_release(vlc_instance);
+
     std::cout << "Hello World!\n";
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
